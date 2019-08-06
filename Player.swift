73d2911d08bbb0cb2ac6cdaad9a48410e9e6a70c
Edy Chan <Jhonchan25@gmail.com>
2019-08-06 17:04:57 +0800

    Update Player.swift

--- Player.swift
+++ Player.swift
@@ -3,7 +3,7 @@
 //  Visbot
 //
 //  Created by CXY on 2019/5/28.
-//  Copyright © 2019 CXY. All rights reserved.
+//  Copyright © 2019 ubt. All rights reserved.
 //
 
 import Alamofire
@@ -20,6 +20,8 @@ enum PlayerStatus {
     case unknown
     case buffering
     case prepareToPlay
+    case pause
+    case continueToPlay
     case finished
     case failed
     case networkError
@@ -30,8 +32,6 @@ class Player: NSObject {
     private var queuePlayer: AVQueuePlayer?
     
     private let reachabilityManager = NetworkReachabilityManager()
-    
-    private(set) var playerItems = [AVPlayerItem]()
     
     private var indexOfPlayingItem = 0
     
@@ -55,37 +55,46 @@ class Player: NSObject {
     
     private var playerStateChanged: PlayerStateChanged?
     
+    private(set) var currentAVPlayerItem: AVPlayerItem?
+    
     private(set) var playerStatus = PlayerStatus.unknown {
         didSet {
+            PlayerLog("\(self) Player state = \(playerStatus)")
             playerStateChanged?(playerStatus)
         }
     }
     
+    private var isPlayingBeforeEnterBackground = false
+    
+    var isInBackground = false
+    
     var isPlaying: Bool {
-        return playerStatus == .prepareToPlay
+        if #available(iOS 10.0, *) {
+            return queuePlayer?.timeControlStatus == .playing
+        } else {
+            // Fallback on earlier versions
+            return queuePlayer?.rate != 0.0
+        }
     }
     
     var itemDuration: Double {
         guard let item = queuePlayer?.currentItem else {
             return 0
         }
-        return CMTimeGetSeconds(item.duration)
+        if item.duration != CMTime.indefinite {
+            return CMTimeGetSeconds(item.duration)
+        }
+        return 0
     }
     
     private var itemTime: Double {
         guard let item = queuePlayer?.currentItem else {
             return 0
         }
-        return CMTimeGetSeconds(item.currentTime())
-    }
-    
-    
-    var itemDurationByMinutes: String {
-        return Int(ceil(itemDuration)).minutesFormatString
-    }
-    
-    var itemTimeByMinutes: String {
-        return Int(ceil(itemTime)).minutesFormatString
+        if item.currentTime() != CMTime.indefinite {
+            return CMTimeGetSeconds(item.currentTime())
+        }
+        return 0
     }
     
     // MARK: 获取播放进度
@@ -110,33 +119,21 @@ class Player: NSObject {
     
     // MARK: 初始化
     
-    override convenience init() {
-        self.init(urlString: nil)
-    }
-
-    convenience init(urlString: String?) {
-        if let url = urlString {
-            self.init(strings: [url])
-        } else {
-            self.init(strings: nil)
-        }
-        
-    }
-    
-    init(strings: [String]?) {
+    override init() {
         super.init()
-        if let strings = strings, let items = itemsForURLs(strings), !items.isEmpty {
-            playerItems = items
-        }
-        queuePlayer = AVQueuePlayer(items: playerItems)
+        queuePlayer = AVQueuePlayer()
         queuePlayer?.actionAtItemEnd = .pause
         if #available(iOS 10.0, *) {
             queuePlayer?.automaticallyWaitsToMinimizeStalling = false
         } else {
             // Fallback on earlier versions
         }
-        
+        // 监听播放完成
+        NotificationCenter.default.addObserver(self, selector: #selector(itemFinished(_:)), name: .AVPlayerItemDidPlayToEndTime, object: queuePlayer?.currentItem ?? nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(itemErrorOccurred(_:)), name: .AVPlayerItemFailedToPlayToEndTime, object: nil)
+        addApplicationObserver()
     }
+    
     
     private func itemsForURLs(_ strings: [String]) -> [AVPlayerItem]? {
         guard !strings.isEmpty else {
@@ -163,38 +160,7 @@ class Player: NSObject {
         }
         return ret
     }
-
-    func appendItems(_ strings: [String]) {
-        if let items = itemsForURLs(strings), !items.isEmpty {
-            playerItems.append(contentsOf: items)
-            guard let player = queuePlayer else { return }
-            items.forEach { (item) in
-                if player.canInsert(item, after: nil) {
-                    player.insert(item, after: nil)
-                }
-            }
-        }
-    }
     
-    func removeItem(_ url: String) {
-        if let items = itemsForURLs([url]), !items.isEmpty {
-            guard let player = queuePlayer else { return }
-            playerItems.removeAll { (item) -> Bool in
-                return item.mc_URL.absoluteString.elementsEqual(url)
-            }
-            let allItems = player.items()
-            for item in allItems {
-                if item.mc_URL.absoluteString.elementsEqual(url) {
-                    player.remove(item)
-                }
-            }
-
-        }
-    }
-    
-    func clearPlayItems() {
-        playerItems.removeAll()
-    }
     
     // MARK: 设置进度
     
@@ -203,123 +169,82 @@ class Player: NSObject {
             finished?(false)
             return
         }
-
+        
         let begintime = CMTimeMake(value: Int64(lround(itemDuration * rate)), timescale: 1)
         player.seek(to: begintime, completionHandler: {(fin) in
             finished?(fin)
         })
     }
     
-    func report(playProgress: ProgressCallBack? = nil, cacheProgress: ProgressCallBack? = nil, currentTime: ProgressCallBack? = nil, duration: ProgressCallBack? = nil, playFinished: PlayBack? = nil) {
+    func report(playProgress: ProgressCallBack? = nil, cacheProgress: ProgressCallBack? = nil, currentTime: ProgressCallBack? = nil, duration: ProgressCallBack? = nil) {
         self.playProgress = playProgress
         self.cacheProgress = cacheProgress
         self.playTimeCallBack = currentTime
-        self.playFinished = playFinished
         self.durationConfirmed = duration
     }
-
     
-    private func internalPlay(atIndex index: Int) {
-        guard let player = queuePlayer, player.items().indices.contains(index) else {
-            PlayerLog("player index error!")
-            return
-        }
-        if index == indexOfPlayingItem + 1 {
-            player.advanceToNextItem()
-            indexOfPlayingItem = index
-            return
-        }
-        player.removeAllItems()
-        let items = playerItems
-        for i in index..<items.count {
-            let item = items[i]
-            if player.canInsert(item, after: nil) {
-                item.seek(to: .zero)
-                player.insert(item, after: nil)
-            }
-        }
-        
-        indexOfPlayingItem = index
-    }
     
     // MARK: 播放
     
-    func play(atIndex index: Int, stateChanged: PlayerStateChanged? = nil) {
-        guard let player = queuePlayer, player.items().indices.contains(index) else {
-            PlayerLog("player index error!")
-            return
-        }
-        playerStateChanged = stateChanged
-        internalPlay(atIndex: index)
-        addObservers()
-    }
-    
     func quickPlay(url: String, stateChanged: PlayerStateChanged? = nil) {
         if let items = itemsForURLs([url]), !items.isEmpty {
-            removeObservers()
-            queuePlayer?.replaceCurrentItem(with: items.first!)
+            queuePlayer?.pause()
+            queuePlayer?.currentItem?.cancelPendingSeeks()
+            queuePlayer?.currentItem?.asset.cancelLoading()
+            removeAVPlayerItemObserver()
+            queuePlayer?.replaceCurrentItem(with: items.first)
+            addAVPlayerItemObserver()
             playerStateChanged = stateChanged
-            addObservers()
         }
     }
     
-    func play() {
-        play(atIndex: 0, stateChanged: nil)
-    }
-
     // MARK: 暂停
     
     func pause() {
         queuePlayer?.pause()
+        playerStatus = .pause
     }
     
     func continuePlay() {
         queuePlayer?.play()
+        playerStatus = .continueToPlay
     }
     
-    private func playNextAndRemoveCurrent() {
-        if let item = queuePlayer?.currentItem {
-            queuePlayer?.remove(item)
-        }
-        playNext()
-    }
-    
-    private func playNext() {
-        internalPlay(atIndex: indexOfPlayingItem + 1)
-    }
     
     // MARK: 停止
-
-    func destroy() {
+    
+    private func destroy() {
         queuePlayer?.pause()
         queuePlayer?.currentItem?.cancelPendingSeeks()
         queuePlayer?.currentItem?.asset.cancelLoading()
-        queuePlayer?.removeAllItems()
-        playerItems.removeAll()
         removeObservers()
     }
-
+    
     
     private func addObservers() {
-        addAVPlayerObserver()
-        self.startApplicationObserver()
+        addAVPlayerItemObserver()
+        addApplicationObserver()
     }
-
+    
     private func removeObservers() {
-        removeAVPlayerObserver()
-        endApplicationObserver()
+        removeAVPlayerItemObserver()
+        removeApplicationObserver()
     }
     
     // MARK: 释放
     
     deinit {
-        PlayerLog("\(self)", #function)
-//        endNetworkReachabilityObserver()
+        PlayerLog("\(self) deinit")
+        //        endNetworkReachabilityObserver()
+        destroy()
+        NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .AVPlayerItemFailedToPlayToEndTime, object: nil)
+        removeApplicationObserver()
     }
 }
 
 extension Player {
-
+    
     // MARK: 监控视频播放进度
     
     private func addPeriodicTimeObserver() {
@@ -341,77 +266,108 @@ extension Player {
     }
     
     // 监听AVPlayerItem对象的status/loadedTimeRanges属性变化，status对应播放状态，loadedTimeRanges网络缓冲状态，当loadedTimeRanges的改变时，每缓冲一部分数据就会更新此属性，可以获得本次缓冲加载的视频范围（包含起始时间、本次网络加载时长）
-    private func addAVPlayerObserver() {
+    private func addAVPlayerItemObserver() {
         guard let item = queuePlayer?.currentItem else { return }
         item.addObserver(self, forKeyPath: "status", options: .new, context: nil)
         item.addObserver(self, forKeyPath: "loadedTimeRanges", options: .new, context: nil)
+        item.addObserver(self, forKeyPath: "playbackLikelyToKeepUp", options: .new, context: nil)
+        item.addObserver(self, forKeyPath: "playbackBufferEmpty", options: .new, context: nil)
         addPeriodicTimeObserver()
-        
-        // 监听播放完成
-        NotificationCenter.default.addObserver(self, selector: #selector(itemFinished(_:)), name: .AVPlayerItemDidPlayToEndTime, object: nil)
     }
+    
+    @objc private func itemErrorOccurred(_ notif: Notification) {
+        guard let currentItem = queuePlayer?.currentItem else { return }
+        if let item = notif.object as? AVPlayerItem, item == currentItem {
+            PlayerLog("\(self) \(currentItem) 播放失败！")
+        }
+    }
+    
+    // MARK: 播放完成
     
     @objc private func itemFinished(_ notif: Notification) {
-        PlayerLog("播放完成！")
-        playerStatus = .finished
-        playTimeCallBack?(itemDuration)
-        // 播放完成回到开始位置
-//        updateProgress(0) { [weak self](ret) in
-//            guard let strongSelf = self else {
-//                self?.playFinished?(true)
-//                return
-//            }
-//            strongSelf.playFinished?(true)
-//        }
+        guard let currentItem = queuePlayer?.currentItem else { return }
+        if let item = notif.object as? AVPlayerItem, item == currentItem, item.isPlaybackLikelyToKeepUp {
+            PlayerLog("\(self) \(String(describing: currentItem.mc_URL)) 播放完成！")
+            playerStatus = .finished
+        }
         
     }
     
-    private func removeAVPlayerObserver() {
+    private func removeAVPlayerItemObserver() {
         guard let item = queuePlayer?.currentItem else { return }
         item.removeObserver(self, forKeyPath: "status")
         item.removeObserver(self, forKeyPath: "loadedTimeRanges")
+        item.removeObserver(self, forKeyPath: "playbackLikelyToKeepUp")
+        item.removeObserver(self, forKeyPath: "playbackBufferEmpty")
         removePeriodicTimeObserver()
-        NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: nil)
     }
     
     // MARK: status & loadedTimeRanges
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        guard let path = keyPath, let dict = change, let duration = queuePlayer?.currentItem?.duration, let item = queuePlayer?.currentItem else { return }
+        guard let path = keyPath, let dict = change, let item = queuePlayer?.currentItem else { return }
         
         if path.elementsEqual("status") {
             if let value = dict[NSKeyValueChangeKey.newKey] as? Int {
                 let status = AVPlayerItem.Status(rawValue: value)
-                if status == AVPlayerItem.Status.readyToPlay {// 显示总时长
-                    queuePlayer?.play()
-                    playerStatus = .prepareToPlay
-                    PlayerLog("---》开始播放")
-                    // 这里获取总时长总是有问题
-                    if duration != CMTime.indefinite {
-                        let total = CMTimeGetSeconds(duration)
-                        PlayerLog("duraion =====  \(total)")
-                    } else {
-                        PlayerLog("duraion =====  NAN")
+                if status == AVPlayerItem.Status.readyToPlay {
+                    if !isInBackground {
+                        queuePlayer?.play()
+                        // redayToPlay状态下获取总时长仍有可能是NAN
+                        playerStatus = .prepareToPlay
+                        PlayerLog("\(self) 开始播放")
                     }
+                    
+                    if item.duration != CMTime.indefinite {
+                        let total = CMTimeGetSeconds(item.duration)
+                        PlayerLog("\(self) duraion: \(total)")
+                    } else {
+                        PlayerLog("\(self) duraion: NAN")
+                    }
+                    
                 } else if status == AVPlayerItem.Status.failed {
-                    PlayerLog("---》播放器状态错误！")
+                    PlayerLog("\(self) 播放器状态错误！")
                     playerStatus = .failed
                 }
             }
             
-        } else if path.elementsEqual("loadedTimeRanges") {// 计算缓存时长
+        } else if path.elementsEqual("loadedTimeRanges") {
+            // 计算缓存时长
             let array = item.loadedTimeRanges
             guard let timeRange = array.first?.timeRangeValue else { return }
             let start = CMTimeGetSeconds(timeRange.start)
             let length = CMTimeGetSeconds(timeRange.duration)
             // 缓冲总长度
             let totalBuffer = start + length
-            let videoLength = CMTimeGetSeconds(duration)
+            let videoLength = CMTimeGetSeconds(item.duration)
             let ratio = totalBuffer/videoLength
-            //  print("缓冲总长度:\(totalBuffer), 视频总长度:\(videoLength), 进度：\(ratio)")
             // 设置缓存进度
             cacheProgress?(ratio)
+        } else if path.elementsEqual("playbackBufferEmpty") {
+            if let playbackBufferEmpty = dict[NSKeyValueChangeKey.newKey] as? Bool {
+                PlayerLog("\(self) playbackBufferEmpty = \(playbackBufferEmpty)")
+                if playbackBufferEmpty {
+                    playerStatus = .buffering
+                }
+            }
+        } else if path.elementsEqual("playbackLikelyToKeepUp") {
+            if let playbackLikelyToKeepUp = dict[NSKeyValueChangeKey.newKey] as? Bool {
+                PlayerLog("\(self) playbackLikelyToKeepUp = \(playbackLikelyToKeepUp)")
+                if playbackLikelyToKeepUp {
+                    if queuePlayer?.rate == 0 && playerStatus != .pause && playerStatus != .finished && playerStatus != .failed && !isInBackground {
+                        PlayerLog("\(self) 缓冲足够可以播放")
+                        queuePlayer?.play()
+                        playerStatus = .continueToPlay
+                    }
+                } else {
+                    if (!NetworkingRequest.shared.isReachable() || NetworkingRequest.shared.isRestricted()) && queuePlayer?.rate == 0 {
+                        queuePlayer?.pause()
+                        playerStatus = .pause
+                    }
+                }
+            }
         }
+        
     }
 }
 
@@ -442,7 +398,6 @@ extension Player {
         
         // start listening
         reachabilityManager?.startListening()
-        startApplicationObserver()
     }
     
     // MARK: 停止网络状态监听
@@ -453,13 +408,13 @@ extension Player {
     
     // MARK: App状态监听
     
-    private func startApplicationObserver() {
+    private func addApplicationObserver() {
         NotificationCenter.default.addObserver(self, selector: #selector(applicaitonEnterBackground(_:)), name: UIApplication.didEnterBackgroundNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(applicaitonEnterForeground(_:)), name: UIApplication.willEnterForegroundNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(avSessionInterrupt(_:)), name: AVAudioSession.interruptionNotification, object: nil)
     }
     
-    private func endApplicationObserver() {
+    private func removeApplicationObserver() {
         NotificationCenter.default.removeObserver(self, name: UIApplication.didEnterBackgroundNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIApplication.willEnterForegroundNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: AVAudioSession.interruptionNotification, object: nil)
@@ -467,11 +422,24 @@ extension Player {
     
     
     @objc private func applicaitonEnterBackground(_ notif: Notification) {
-        
+        PlayerLog("\(self) applicaitonEnterBackground")
+        if isPlaying {
+            queuePlayer?.pause()
+            playerStatus = .pause
+            isPlayingBeforeEnterBackground = true
+            PlayerLog("\(self) 进入后台暂停播放")
+        } else {
+            isPlayingBeforeEnterBackground = false
+        }
     }
     
     @objc private func applicaitonEnterForeground(_ notif: Notification) {
-        
+        PlayerLog("\(self) applicaitonEnterForeground")
+        if isPlayingBeforeEnterBackground {
+            queuePlayer?.play()
+            playerStatus = .continueToPlay
+            PlayerLog("\(self) 进入前台恢复播放")
+        }
     }
     
     
